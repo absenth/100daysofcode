732ac437c396b9958e3c37b34a49655a822d8b20
absenth <wintermute223@gmail.com>
2021-06-18 12:55:16 -0500

    Day 3 100 Days of Code - Choose Your Own Adventure

---
+++ day3/README.md
@@ -0,0 +1,9 @@
+# 100 Days Of Code - Python
+## Day 3 Project: Choose Your Own Adventure
+
+### Requirements:
+1. use nested if for flow control
+2. Greet the user
+3. Left or right? - Right = game over
+4. Swim or wait? - swim = game oveer
+5. Which Door? - red or blue = game over
---
+++ day3/main.py
@@ -0,0 +1,68 @@
+# 100DaysOfCode - Day 3 Choose Your Own Adventure
+
+
+def main():
+    """ Riviting function description here """
+    print("Welcome to Day 3 of 100DaysOfCode")
+    print("")
+    print("You come to a fork in the road.")
+    print("Do you choose the Left or Right path?")
+    if not left_or_right():
+        print("You have been eaten by a Grue!")
+        dead()
+        exit()
+
+    print("You arrive at a river.  Swim or Wait for a boat?")
+    if not sink_or_swim():
+        print("A rogue river Grue spots you swimming.")
+        print("You do your best to swim faster trying to escape.")
+        print("You have been eaten bya a Grue!")
+        dead()
+        exit()
+
+    print("You arrive at a castle with three doors.")
+    print("You must choose.  Red, Blue, or Yellow.")
+    if not red_blue_or_yellow():
+        print("You come face to face with a large Grue!")
+        dead()
+        exit()
+
+    print("You enter the Yellow door.")
+    print("Inside is a cheeseburger.")
+    print("You Win!")
+
+
+def dead():
+    """ Function for when you fail! """
+    print("You are dead! Rigor Mortis Habeas Corpus")
+
+
+def left_or_right():
+    """ Function for First Option """
+    l_or_r = input("L or R? ")
+    if l_or_r.lower().strip() == "l":
+        return True
+    else:
+        return False
+
+
+def sink_or_swim():
+    """ Do we swim or wait for the boat """
+    wait_or_swim = input("Wait or Swim? ")
+    if wait_or_swim.lower().strip() == "w":
+        return True
+    else:
+        return False
+
+
+def red_blue_or_yellow():
+    """ Pick a door, any door """
+    pick_a_door = input("Red, Blue or Yellow? ")
+    if pick_a_door.lower().strip() == "y":
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()
+
